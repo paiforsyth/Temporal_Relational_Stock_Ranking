5f125e1562f4c6fde452a6624888203acac1b251
Peter Forsyth <peterforsyth@MacBook-Pro-2.local>
2019-01-12 12:12:19 -0500

    3

--- training/rstat.py
+++ training/rstat.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import itertools
 import copy
 from tqdm import tqdm
@@ -34,41 +35,103 @@ def get_index_to_industry( data_path ='../data', market_name = "NASDAQ" ):
     return np.load(rpath).item() #since dict is stored in array
 
 
+
+def compute_relationship_correlations(abs_cor_mat, relationship_matrix):
+    dim = len(abs_cor_mat)
+    rr_mat = relationship_matrix * np.tri(dim,dim,-1)
+    dex1, dex2 = rr_mat.nonzero()
+    cor_vec = abs_cor_mat[dex1,dex2]
+    return cor_vec
+
+def get_wikidata_correltions(abs_cor_mat):
+    num_stocks = len(abs_cor_mat)
+    r_tensor = get_relation_data(relation_name="wikidata")
+    index_to_industry = get_index_to_industry()
+    index_to_correlation ={}
+    for index in index_to_industry:
+        r_tensor_slice = r_tensor[:,:,index] #slice of relationships pertaining to a given industry
+        cor = compute_relationship_correlations(abs_cor_mat, r_tensor_slice)
+          
+
+
 def get_industry_correlatons(abs_cor_mat):
     assert np.all(abs_cor_mat >= 0)
     num_stocks = len(abs_cor_mat)
     r_tensor = get_relation_data()
     index_to_industry = get_index_to_industry()
     index_to_correlation ={}
-    max_cor=-10
-    max_cor_dex =-10
     for index in index_to_industry:
         # plt.matshow(r_tensor[:,:,index])
         # plt.show()
         r_tensor_slice = r_tensor[:,:,index] #slice of relationships pertaining to a given industry
 
         v = np.diag(r_tensor_slice) 
-        pair_indexes = list(zip(*list(itertools.combinations(v.nonzero()[0].tolist(),2 )))) 
+        company_indexes = v.nonzero()[0]#since return is a tuple
+        pair_indexes = list(zip(*list(itertools.combinations(company_indexes.tolist(),2 )))) 
         #import pdb; pdb.set_trace()
 
         cors = abs_cor_mat[pair_indexes[0], pair_indexes[1]] #1d array consisting the of the correlations between
         industry = index_to_industry[index]
-        index_to_correlation[index] = {"industry":industry, "correlations": cors, "mean": np.mean(cors), "std": np.std(cors)  }
-        if index_to_correlation[index]["mean"] > max_cor:
-            max_cor = index_to_correlation[index]["mean"]
-            max_cor_dex = index 
-
+        index_to_correlation[index] = {"industry":industry, "correlations": cors, "mean": np.mean(cors), "std": np.std(cors), "company_indexes": company_indexes, "num_companies": sum(v) }
+        # if index_to_correlation[index]["mean"] > max_cor:
+            # max_cor = index_to_correlation[index]["mean"]
+            # max_cor_dex = index 
+    
+    #add an industry consisting of all stocks
     flat_cor_mat = abs_cor_mat.reshape(-1)
-    index_to_correlation[-1] = {"industry": "all", "correlations": flat_cor_mat, "mean": np.mean(flat_cor_mat), "std": np.std(flat_cor_mat)  }
-    mi = index_to_correlation[max_cor_dex]
+    index_to_correlation[-1] = {"industry": "all", "correlations": flat_cor_mat, "mean": np.mean(flat_cor_mat), "std": np.std(flat_cor_mat) , "company_indexes": list(range(num_stocks)), "num_companies": num_stocks }
+
+    #compute data for related and unrelated stocks
+    related = np.sum(r_tensor, axis=2)
+    related[related>0] = 1
+    r_cors = compute_relationship_correlations(abs_cor_mat, related)
+    index_to_correlation[-2] =  {"industry": "related", "correlations": r_cors, "mean": np.mean(r_cors), "std": np.std(r_cors) , "company_indexes":float("NaN") , "num_companies": float("NaN") }
+    print("mean absolute correlation of related stocks is {0:.4f} with std {1:.4f}".format(np.mean(r_cors), np.std(r_cors)))
+
+    unrelated = np.ones(related.shape) - related
+    u_cors = compute_relationship_correlations(abs_cor_mat, unrelated)
+    index_to_correlation[-3] =  {"industry": "related", "correlations": u_cors, "mean": np.mean(u_cors), "std": np.std(u_cors) , "company_indexes":float("NaN") , "num_companies": float("NaN") }
+    print("mean absolute correlation of unrelated stocks is {0:.4f} with std {1:.4f}".format(np.mean(u_cors), np.std(u_cors) ))
+
+
+
+    
+
+    # import pdb; pdb.set_trace()
+    df = pd.DataFrame(index_to_correlation).transpose()
+    # mi = index_to_correlation[max_cor_dex]
     ma = index_to_correlation[-1]
-    print("maximum correlation is the {} industry which has mean {} std {} ".format(mi["industry"], mi["mean"], mi["std"] ))
-    print("overall correlation is mean {} std {}".format(ma["mean"], ma["std"] ))
+    # print("maximum correlation is the {} industry which has mean {} std {} ".format(mi["industry"], mi["mean"], mi["std"] ))
+    print("overall correlation is mean {0:.4f} std {1:.4f}".format(ma["mean"], ma["std"] ))
 
 
-    return index_to_correlation
+    df = df.sort_values(by = "mean", ascending=False)
+    return df
 
 
+
+def plot_industry_data(index, d,legend=True, market_name=""):
+    index_to_industry = get_index_to_industry()
+    industry_name = index_to_industry[index]
+    r_tensor = get_relation_data()
+    r_tensor_slice = r_tensor[:,:,index] #slice of relationships pertaining to a given industry
+    v = np.diag(r_tensor_slice) 
+    company_indexes = v.nonzero()[0]#since return is a tuple
+    price_data = d["eod_data"][:,:,-1]
+    industry_price_data = price_data[company_indexes ,:]
+    industry_price_data[industry_price_data == 1.1 ] =float("NaN") #This is the dummy value for missing days
+    tickers = d["tickers"]
+    labels = [tickers[dex] for dex in company_indexes  ]
+    # import pdb; pdb.set_trace()
+    fig, ax = plt.subplots()
+    for i in range(len(industry_price_data)) :
+        ax.plot(industry_price_data[i,:])
+
+    # ax.plot(y=industry_price_data.transpose())
+    if legend:
+        ax.legend(labels)
+    plt.title(market_name + industry_name)
+    plt.show()
 
 
 
@@ -91,6 +154,7 @@ def make_stock_correlation_matrix(d):
             val = correlate_returns(d,i,j)
             cor_mat[i,j] = val
             cor_mat[j,i] = val
+    import pdb; pdb.set_trace()
     return cor_mat
 
 def write_stock_correlation_matrix(d):
@@ -147,11 +211,43 @@ def test_cor_matshow(fname = "test_eod_file"):
     plt.matshow(d["masks"])
     plt.show()
 
+def test_plot( fname = "test_eod_file" ):
+     with open(fname,'rb') as f:
+        d = pickle.load(f)
+     plot_industry_data(32, d, legend=True, market_name = "NASDAQ_")
+    
+
 def test_cor_compute(fname = "test_eod_file"):
     with open(fname,'rb') as f:
         d = pickle.load(f)
     cor_mat = correlate_returns(d,0,50)
     
+def test_industry_cor(fname="test_eod_file"):
+    with open(fname,'rb') as f:
+        d = pickle.load(f)
+    mat = np.load("../cor_data/correlation_matrix.npy")
+    abs_cor_mat = abs(mat)
+    # import pdb; pdb.set_trace()
+    df = get_industry_correlatons(abs_cor_mat)
+    # print(df.loc[:,["industry", "mean", "num_companies"]])
+    df.to_excel("../cor_data/industry/NASDAQ.xlsx")
+    # fig,ax = plt.subplots()
+    # ax.hist(df.loc[-1,"correlations"] )
+    # plt.title("all corelations")
+
+    # fig,ax = plt.subplots()
+    # ax.hist(df.loc[-2,"correlations"] )
+    # plt.title("related corelations")
+
+    # fig,ax = plt.subplots()
+    # ax.hist(df.loc[-3,"correlations"] )
+    # plt.title("unrelated corelations")
+
+    # plt.show()
+    
+
+    
+    return df
     
 
 
@@ -173,14 +269,18 @@ def test_fm_industry_cor(fname="test_eod_file"):
         d = pickle.load(f)
     mat = make_fully_masked_correlation_matrix(d)
     abs_cor_mat = abs(mat)
-    index_to_cor = get_industry_correlatons(abs_cor_mat)
+    df = get_industry_correlatons(abs_cor_mat)
+    return df
     #print(index_to_cor)
  
     
 
 
 if  __name__ == "__main__":
-    test_fm_industry_cor()
+    test_plot()
+    # df = test_industry_cor()
+    # import pdb; pdb.set_trace()
+    # df = test_fm_industry_cor()
     # logging.getLogger().setLevel(logging.INFO)
     # get_relation_data()
     # test_fm_cor_mat()
@@ -188,3 +288,4 @@ if  __name__ == "__main__":
     # test_cor_load()
     # test_cor_compute()
     #print(get_industry_names())
+    # test_cor_mat()
